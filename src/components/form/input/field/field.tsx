b147b70cfb391353098a9b09988e1e436a435e45
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-09 17:03:53 +0200

    Input update state management

--- src/components/form/input/field/field.tsx
+++ src/components/form/input/field/field.tsx
@@ -11,6 +11,8 @@
  * @member errorMessage - Input error message, when it is null or undefined, no error considered
  */
 
+import { ChangeEvent, ChangeEventHandler, useEffect, useState } from "react";
+
 export interface InputFieldProps {
     id?: string;
     label?: string;
@@ -21,12 +23,30 @@ export interface InputFieldProps {
     onChange?: (value: string) => void;
 }
 
+interface InputStateProps {
+    value?: string;
+}
+
 /**
+ * A controlled input element, with much features
  * 
  * @param props 
  * @returns Controlled input element
  */
 export const InputField = (props: InputFieldProps) => {
+
+    const [inputState, setInputState] = useState<InputStateProps>({ value: props.defaultValue });
+
+    const handleInputValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setInputState(state => ({ ...state, value }));
+        props.onChange?.(value);
+    };
+
+    useEffect(() => {
+        setInputState(state => ({ ...state, value: props.value }));
+    }, [props.value])
+
     return (
         <div className="text-start">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={props.id}>
@@ -38,6 +58,8 @@ export const InputField = (props: InputFieldProps) => {
                  ${props.errorMessage ? 'border-red-600' : 'border-secondary'}`}
                 id={props.id}
                 type="text"
+                value={inputState.value}
+                onChange={handleInputValueChange}
                 placeholder={props.label} />
 
             {props.errorMessage && <p className="text-red-500 text-xs italic mt-1">{props.errorMessage}</p>}
