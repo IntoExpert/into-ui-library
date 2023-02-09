11c067792fb31de095c8ed6379b35d6177c238a4
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-09 18:07:36 +0200

    Test update

--- src/components/form/input/field/Field.test.tsx
+++ src/components/form/input/field/Field.test.tsx
@@ -3,24 +3,26 @@ import { shallow, ShallowWrapper } from 'enzyme';
 
 describe("Input field functionality", () => {
     let inputField: ShallowWrapper, inputElement: ShallowWrapper;
-    beforeEach(() => {
-        inputField = shallow(<InputField defaultValue="Motasem" />);
-        inputElement = inputField.find('input');
-    });
-    it("Render without crashing", () => {
-        shallow(<InputField />);
-    });
+    describe("Basice functionality and state", () => {
+        beforeEach(() => {
+            inputField = shallow(<InputField defaultValue="Motasem" />);
+            inputElement = inputField.find('input');
+        });
+        it("Render without crashing", () => {
+            shallow(<InputField />);
+        });
 
-    it("Reflect the default value", () => {
-        const value = inputElement.prop('value');
-        expect(value).toEqual("Motasem");
-    });
+        it("Reflect the default value", () => {
+            const value = inputElement.prop('value');
+            expect(value).toEqual("Motasem");
+        });
 
-    it("Value is changed", () => {
-        inputElement.simulate('change', { target: { value: "New value" } });
-        const newInput = inputField.find('input');
-        const value = newInput.prop('value');
-        expect(value).not.toEqual("Motasem");
-        expect(value).toEqual("New value");
+        it("Value is changed", () => {
+            inputElement.simulate('change', { target: { value: "New value" } });
+            const newInput = inputField.find('input');
+            const value = newInput.prop('value');
+            expect(value).not.toEqual("Motasem");
+            expect(value).toEqual("New value");
+        })
     })
 });
