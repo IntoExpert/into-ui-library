b46b8812fdc0271544bba3fa93e3dbadd83090c0
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-09 15:18:51 +0200

    Package config

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const dts = require("rollup-plugin-dts");
+const terser = require("@rollup/plugin-terser");
+const peerDepsExternal = require('rollup-plugin-peer-deps-external');
+
+const packageJson = require("./package.json");
+
+module.exports = [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: packageJson.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            typescript({
+                tsconfig: "./rollup.tsconfig.json"
+            }),
+            commonjs({
+                include: ["node_modules/**"],
+                namedExports: {
+                    "node_modules/react/react.js": [
+                        "Children",
+                        "Component",
+                        "PropTypes",
+                        "createElement"
+                    ],
+                    "node_modules/react-dom/index.js": ["render"]
+                }
+            })
+        ]
+    }
+];
