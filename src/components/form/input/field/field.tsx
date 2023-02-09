57dbd5b30375f7f2cb7e8fcc1bb899bf950dd60f
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-09 16:41:26 +0200

    Field customization

--- src/components/form/input/field/field.tsx
+++ src/components/form/input/field/field.tsx
@@ -1,12 +1,31 @@
+/**
+ * Input field properties
+ * 
+ * @interface InputFieldProps
+ * @member id - The id of the input element
+ * @member label - Input label, if you leave it empty no label will show
+ * @member placeholder - Input placeholder
+ * @member value - Input current value, it will be sync to the input element
+ * @member defaultValue - Input initial value
+ * @member onChange - On input value change callback
+ * @member errorMessage - Input error message, when it is null or undefined, no error considered
+ */
+
 export interface InputFieldProps {
     id?: string;
     label?: string;
     placeholder?: string;
     value?: string;
     defaultValue?: string;
+    errorMessage?: string;
     onChange?: (value: string) => void;
 }
 
+/**
+ * 
+ * @param props 
+ * @returns Controlled input element
+ */
 export const InputField = (props: InputFieldProps) => {
     return (
         <div className="text-start">
@@ -14,11 +33,14 @@ export const InputField = (props: InputFieldProps) => {
                 {props.label}
             </label>
             <input className={`shadow appearance-none border 
-                border-secondary rounded-lg py-2 px-3 text-grey-700 leading-tight 
-                 focus:outline-none focus:shadow-outline`}
+                 rounded-lg py-2 px-3 text-grey-700 leading-tight 
+                 focus:outline-none focus:shadow-outline
+                 ${props.errorMessage ? 'border-red-600' : 'border-secondary'}`}
                 id={props.id}
                 type="text"
                 placeholder={props.label} />
+
+            {props.errorMessage && <p className="text-red-500 text-xs italic mt-1">{props.errorMessage}</p>}
         </div>
     );
 }
