f243216f101b01430efe3a38690737af2772e0a5
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-14 17:00:04 +0200

    Forms

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,19 +1,49 @@
-import { FormEvent, useMemo, useState } from "react";
 import { InputField, InputFieldProps } from "./input/field/field";
-import { FieldValues, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 export interface FormFieldProps extends InputFieldProps {
-    validation: {
+    /**
+     * Validation logic
+     */
+    validation?: {
+        /**
+         * True if the field is required
+         */
         required?: boolean;
+        /**
+         * Custom validation function
+         * 
+         * @param value 
+         * @returns false if the field is not valid
+         */
         validate?: (value: any) => boolean;
     };
+    /**
+     * HTML input element type
+     * 
+     */
     elementType?: "input" | "select" | "textarea";
 };
 
 export interface FormProps<TFieldValues> {
+    /**
+     * Form id
+     */
     id?: string;
+    /**
+     * Inputs default values
+     */
     defaultValues?: any;
+    /**
+     * On form submit event
+     * 
+     * @param values 
+     * @returns form values
+     */
     onSubmit: (values: TFieldValues) => void;
+    /**
+     * Input fields structure, to build your dynamic form
+     */
     fields?: FormFieldProps[] | undefined;
 };
 
@@ -43,12 +73,10 @@ export const DynamicForm = function <TFieldValues>(props: FormProps<TFieldValues
     }
 
     return (
-        <form className="row space-y-3" onSubmit={handleSubmit(props.onSubmit)}>
+        <form id={props.id} className="row space-y-3" onSubmit={handleSubmit(props.onSubmit)}>
             {props.fields?.map((field: FormFieldProps, index: number) => {
-                return <Input key={field.name} {...field} />;
+                return <Input key={index} {...field} />;
             })}
-
-
         </form>
     );
 };
