9e707eb0f4f65dd57e591b158d88dccc3e1f1b95
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-16 10:39:19 +0200

    Add type attribute for button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,16 +1,36 @@
 import { MouseEvent } from "react";
 
 export interface ButtonProps {
+    /**
+     * Button tooltip title
+     */
     title?: string;
+    /**
+     * Set button disabled
+     */
     disabled?: boolean;
+    /**
+     * Button type, ["button", "reset", "submit"]
+     */
+    type?: "button" | "reset" | "submit";
+    /**
+     * On button click
+     * 
+     * @param event 
+     * @returns 
+     */
     onClick?: (event: MouseEvent) => void;
-    children?: JSX.Element;
+    /**
+     * Button content
+     */
+    children?: JSX.Element | string | undefined;
 };
 
-export const Button = ({ title, disabled, onClick, children }: ButtonProps) =>
+export const Button = ({ title, disabled, onClick, children, type }: ButtonProps) =>
 (<button
     className={`bg-primary text-white font-bold rounded px-3 py-2`}
     title={title}
+    type={type}
     onClick={onClick}>
     {children}
 </button>);
