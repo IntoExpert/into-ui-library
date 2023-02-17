80aa3e0ad8eb97adbb1939c40ff0da45903a14b6
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-17 13:08:37 +0200

    Images

--- src/stories/image/image.stories.tsx
+++ src/stories/image/image.stories.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { DynamicForm } from '../../components';
 import "../../index.css";
 import { Avatar } from '../../components/image';
 
