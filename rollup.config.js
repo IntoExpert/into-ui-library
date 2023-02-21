ffcb2b353a4c8bf901fb896d0453bcf5c64f711b
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-21 12:59:29 +0200

    Build enhancements

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,17 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
 const peerDepsExternal = require('rollup-plugin-peer-deps-external');
-const babel = require('@rollup/plugin-babel');
 const packageJson = require("./package.json");
 const { default: terser } = require("@rollup/plugin-terser");
 
 module.exports = [
     {
         input: "src/components/index.ts",
+        external: [
+            'react',
+            'react-dom'
+        ],
         output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
             {
                 file: packageJson.module,
                 format: "esm",
@@ -28,10 +26,6 @@ module.exports = [
                 tsconfig: "./rollup.tsconfig.json",
             }),
             commonjs(),
-            babel({
-                exclude: 'node_modules/**',
-                babelHelpers: 'bundled',
-            }),
             terser()
         ],
     }
