ea4a7557ee40546aa7a4b66f3b6279917b2f5a71
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-24 15:12:11 +0200

    Update animation

--- src/components/camera/camera.tsx
+++ src/components/camera/camera.tsx
@@ -12,6 +12,7 @@ export const Camera = (props: CameraProps) => {
 
     const webcamRef = useRef<Webcam>(null);
 
+    /** Take user photo */
     const capturePhoto = (event: MouseEvent) => {
         event.stopPropagation();
         const src = webcamRef?.current?.getScreenshot()
@@ -21,10 +22,12 @@ export const Camera = (props: CameraProps) => {
         props.onCapture?.(src);
     };
 
+    /** Camera capture click button */
     const CaptureClick = () => {
-        return <div className={`w-14 h-14 bg-white bg-opacity-80 transition rounded-full flex justify-center items-center 
-        absolute bottom-10 left-1/2 -translate-x-1/2 group hover:bg-opacity-70 `}>
-            <div className={`min-w-[3rem] min-h-[3rem] bg-white rounded-full transition group-hover:min-w-[2.5rem] group-hover:min-h-[2.5rem] hover:hue-rotate-180`}>
+        return <div className={`w-16 h-16 border-4 border-white border-opacity-80 bg-white bg-opacity-80 transition rounded-full flex justify-center items-center 
+        absolute bottom-10 left-1/2 -translate-x-1/2 group hover:bg-opacity-70 hover:border-opacity-60 hover:border-b-transparent hover:border-t-transparent hover:rotate-180 `}>
+            <div className={`min-w-[3rem] min-h-[3rem] bg-white rounded-full transition group-hover:min-w-[2.5rem] 
+            group-hover:min-h-[2.5rem] hover:hue-rotate-180`}>
             </div>
         </div>
     }
