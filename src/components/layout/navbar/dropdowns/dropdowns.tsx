08fd80ca02ea642fa08c6a07651a27c2c70a6ec5
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-28 15:33:27 +0200

    Fix dropdowns spacing

--- src/components/layout/navbar/dropdowns/dropdowns.tsx
+++ src/components/layout/navbar/dropdowns/dropdowns.tsx
@@ -9,7 +9,12 @@ export const NavbarDropdowns = (props: NavbarDropdownProps) => {
 
     return (
         <div className={`flex items-center${props.className ?? ''}`}>
-            {props.dropdowns?.map((dropdown, index) => (<Dropdown key={index} {...dropdown} />))}
+            {props.dropdowns?.map((dropdown, index) => (
+                <div key={index}>
+                    {index > 0 ? <span className={`mx-1`}></span> : null}
+                    <Dropdown {...dropdown} />
+                </div>
+            ))}
         </div>
     )
 }
