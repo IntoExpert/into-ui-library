f4fef252cf2c8b20f552bc7be4397272c89bf79b
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-28 10:16:03 +0200

    Refactor badge

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -9,9 +9,6 @@ export const Badge = ({ children, className }: badge): ReactElement => {
 
     return (
         <div className={`relative w-16 bg-secondary text-white ${className ?? ''}`}>
-            <div className={`absolute w-3/4 bg-secondary rotate-45 scale-1 top-1/2`}>
-
-            </div>
             {children}
         </div>
     );
