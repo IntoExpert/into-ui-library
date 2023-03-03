1c960c1e0a988456398c46d7ad7773b1ba78a995
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-03 11:57:55 +0200

    Fix upload bg opacity

--- src/components/form/input/fileUpload/retakeButton/retakeButton.tsx
+++ src/components/form/input/fileUpload/retakeButton/retakeButton.tsx
@@ -3,7 +3,7 @@ import { Button, ButtonProps } from "../../../../button";
 export const RetakeButton = (props: ButtonProps) => {
 
     return (
-        <Button {...props} className={`!bg-primary border border-primary bg-opacity-40 ${props.className ?? ''}`}>
+        <Button {...props} className={`!bg-primary border border-primary !bg-opacity-40 ${props.className ?? ''}`}>
             {props.children}
         </Button>
     );
