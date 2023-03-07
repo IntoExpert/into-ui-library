772ad5f30044298c02b5e3586f5504ee04a511ff
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-07 16:13:46 +0200

    colorable checkmark

--- src/components/form/input/field/field.tsx
+++ src/components/form/input/field/field.tsx
@@ -99,9 +99,9 @@ export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(({ value
             const radioClasses = `appearance-none rounded-full border border-gray-300 bg-white checked:bg-white 
             checked:border-secondary checked:border-4 focus:outline-secondary transition duration-300 
             cursor-pointer focus:ring-2 focus:ring-secondary focus:shadow-none`;
-            const checkBoxClasses = `rounded border border-gray-400 checked:bg-red-400 checked:after:content-['✔'] 
+            const checkBoxClasses = `rounded border border-gray-400 checked:bg-red-400 checked:after:content-['✓'] 
             checked:border-red-400 after:text-surface after:absolute after:0 after:w-full after:flex after:justify-center 
-            after:items-center after:text-[0.5rem] appearance-none focus:outline-secondary`;
+            after:items-center after:text-xs appearance-none focus:outline-secondary`;
 
             return (
                 <div className={`flex justify-start align-baseline ${props.containerClassName}`}>
