bd2d6a92a58d118fc7ee6fab66abb5896ff83a6f
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-07 07:40:00 +0200

    Update icon button

--- src/components/button/icon/iconButton.tsx
+++ src/components/button/icon/iconButton.tsx
@@ -3,11 +3,15 @@ import { Button, ButtonProps } from "../button";
 
 export interface IconButtonProps extends ButtonProps {
     icon?: ReactElement;
+    withoutBg?: boolean;
 };
 
-export const IconButton = (props: IconButtonProps) => {
+export const IconButton = ({ icon, withoutBg, className, ...props }: IconButtonProps) => {
 
-    return (<Button {...props} className={`rounded-full aspect-square !p-1 fill-surface ${props.className ?? ''}`}>
-        {props.icon}
+    return (<Button {...props}
+        className={`rounded-full aspect-square !p-1 fill-surface 
+        ${withoutBg ? 'bg-transparent hover:bg-primaryVariant' : ''} 
+        ${className ?? ''}`}>
+        {icon}
     </Button>)
 }
