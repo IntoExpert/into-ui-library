8831ca4093cffa3f57eba2b9d140af0db0525aa8
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-08 12:38:13 +0200

    Add bg classname

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -9,14 +9,15 @@ export interface ModalProps extends UiElementProps {
     onClose?: () => void;
     children?: ReactElement;
     size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "full";
+    bgClassname?: string;
 };
 
-export const Modal = ({ show, hasCloseButton, onClose, children, size, className }: ModalProps) => {
+export const Modal = ({ show, hasCloseButton, onClose, children, size, className, bgClassname }: ModalProps) => {
 
     if (!show || typeof window !== "object") return null;
 
     return createPortal(
-        <div className={`absolute w-screen h-screen top-0 flex justify-center items-center backdrop-blur-sm`}>
+        <div className={`absolute w-screen h-screen top-0 flex justify-center items-center ${bgClassname ?? ''}`}>
             <div className={`${size ? (`max-w-${size}`) : ''}`}>
                 <section className={`relative w-full bg-surface ${className}`}>
                     {hasCloseButton && <IconButton className={`absolute top-1 right-1 `} icon={<span>&times;</span>} onClick={onClose}></IconButton>}
