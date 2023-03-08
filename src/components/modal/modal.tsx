b6df12dac3e3bf3ddd74503d0aaeadceb5c1d1eb
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-08 12:30:31 +0200

    Add modal

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,26 @@
+import { createPortal } from "react-dom";
+import { ReactElement } from "react";
+import { IconButton, UiElementProps } from "@intoexpert/into-ui-library";
+
+export interface ModalProps extends UiElementProps {
+    show?: boolean;
+    hasCloseButton?: boolean;
+    onClose?: () => void;
+    children?: ReactElement;
+};
+
+export const Modal = ({ show, hasCloseButton, onClose, children, className }: ModalProps) => {
+
+    if (!show) return null;
+
+    return createPortal(
+        <div className={`absolute w-screen h-screen top-0 flex justify-center items-center backdrop-blur-sm`}>
+            <div className={`max-w-lg`}>
+                <section className={`relative w-full bg-surface ${className}`}>
+                    {hasCloseButton && <IconButton className={`absolute top-1 right-1 `} icon={<span>&times;</span>} onClick={onClose}></IconButton>}
+                    {children}
+                </section>
+            </div>
+        </div>
+        , document.body)
+}
