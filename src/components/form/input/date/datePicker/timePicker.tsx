5429ad25423e63c203d880b4205f5dc2e9a98149
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-08 19:52:23 +0200

    Add error message label etc

--- src/components/form/input/date/datePicker/timePicker.tsx
+++ src/components/form/input/date/datePicker/timePicker.tsx
@@ -2,24 +2,46 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./style.scss";
 import ReactDatePicker, { ReactDatePickerProps } from "react-datepicker";
 import { UiElementProps } from "../../../../common";
-import { ChangeEvent, useCallback, useState } from "react";
+import { InputLabel } from "../../label";
+import { ReactElement } from "react";
 
 export interface TimePickerProps extends UiElementProps, ReactDatePickerProps {
-
+    /**
+     * Input label, if you leave it empty no label will show
+     */
+    label?: ReactElement | string;
+    /**
+     * Css classses for input label container
+     */
+    containerClassName?: string;
+    /**
+     * Input error message, when it is null or undefined, no error considered
+     */
+    errormessage?: string;
 };
 
 export const TimePicker = ({ className, value, ...props }: TimePickerProps) => {
 
-    return (<ReactDatePicker
-        calendarStartDay={1}
-        showTimeSelect
-        showTimeSelectOnly
-        timeIntervals={30}
-        dateFormat={`hh:mm aa`}
-        {...props}
-        className={`timpicker shadow appearance-none border border-secondary
+    return (
+        <div className={`text-start ${props.containerClassName}`}>
+            <InputLabel inputId={props.id} content={props.label} className="block mb-2" />
+            <div className={`relative`}>
+                <ReactDatePicker
+                    calendarStartDay={1}
+                    showTimeSelect
+                    showTimeSelectOnly
+                    timeIntervals={30}
+                    dateFormat={`hh:mm aa`}
+                    {...props}
+                    className={`timpicker shadow appearance-none border border-secondary
             rounded h-10 w-24 px-3 text-gray-700 leading-tight 
             focus:outline-none focus:shadow-outline
-          placeholder:text-gray-500 ${className ?? ''}`}
-    />);
+          placeholder:text-gray-500 ${props.errormessage ? 'border-red-400' : 'border-secondary'} 
+          ${className ?? ''}`}
+                />
+            </div>
+
+            {props.errormessage && <p className="text-red-400 text-xs italic mt-1">{props.errormessage}</p>}
+        </div>
+    );
 }
