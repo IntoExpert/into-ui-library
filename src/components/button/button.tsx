fd5016e5c8e2e213e18f5b46b5e106c25337e8fb
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-11 19:51:23 +0200

    disabled buttons

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -30,7 +30,7 @@ export interface ButtonProps extends UiElementProps {
 export const Button = ({ title, disabled, onClick, children, type, className }: ButtonProps) =>
 (<button
     className={`bg-secondary text-xs transition-colors hover:bg-primary text-white rounded px-3 py-2 
-    disabled:bg-gray-100 disabled:text-gray-800 ${className ?? ''}`}
+    disabled:!bg-gray-100 disabled:pointer-events-none disabled:text-gray-800 ${className ?? ''}`}
     title={title}
     type={type}
     onClick={onClick}
