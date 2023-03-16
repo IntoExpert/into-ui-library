21c95a315657dd6297aa38f0be389be67c812c34
hasan <hasan>
2023-03-16 17:11:47 +0200

    changes

--- src/components/form/input/authInput/dateInput.tsx
+++ src/components/form/input/authInput/dateInput.tsx
@@ -15,7 +15,11 @@ export const DateInput = (props: DateInputProps) => {
         type={`date`}
         className="placeholder: pb-[2px] rounded-lg w-96 text-sm z-10  "
       />
-      <div className={`fill-yellow-500 absolute  ${props.icon ?? " "}`}>
+      <div
+        className={`fill-yellow-500 absolute left-[350px] top-[15px] ${
+          props.icon ?? " "
+        }`}
+      >
         {props.icon}
       </div>
     </div>
