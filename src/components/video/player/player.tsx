4b2e2a173213e4db47c21c9dffd6208faaa7db10
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-19 13:40:59 +0200

    Play button position fixes

--- src/components/video/player/player.tsx
+++ src/components/video/player/player.tsx
@@ -18,7 +18,7 @@ export const VideoPlayer = ({ showPlayButton, playButtonPosition = 'center', ...
     }
         , [play])
     return (
-        <div className={`relative`}>
+        <div className={`relative w-fit`}>
             <ReactPlayer {...props} playing={showPlayButton ? play : props.playing} />
             {showPlayButton
                 ? <span
