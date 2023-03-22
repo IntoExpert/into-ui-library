17b89c80d767df533128f6cf97a5243e338729a1
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-22 14:51:46 +0200

    Add id to card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,10 +2,11 @@ import { ReactElement } from "react";
 import { UiElementProps } from "../common";
 
 export interface CardProps extends UiElementProps {
+    id?: string;
     children?: ReactElement;
 };
 
-export const Card = ({ children, className }: CardProps) => {
+export const Card = ({ id, children, className }: CardProps) => {
     return (
         <article className={`shadow rounded-lg overflow-clip bg-surface ${className ?? ''}`}>
             {children}
