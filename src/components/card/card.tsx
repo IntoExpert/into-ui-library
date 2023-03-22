9528e879be4099c78f17d72d6ec4c0c2eb10afaf
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-22 14:50:40 +0200

    Reset

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,12 +2,13 @@ import { ReactElement } from "react";
 import { UiElementProps } from "../common";
 
 export interface CardProps extends UiElementProps {
+    id?: string;
     children?: ReactElement;
 };
 
-export const Card = ({ children, className }: CardProps) => {
+export const Card = ({ id, children, className }: CardProps) => {
     return (
-        <article className={`shadow rounded-lg overflow-clip bg-surface ${className ?? ''}`}>
+        <article id={id} className={`shadow rounded-lg overflow-clip bg-surface ${className ?? ''}`}>
             {children}
         </article>
     );
