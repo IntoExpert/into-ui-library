32f572653d619f3c10af5581601d909adf596905
hasan <hasan>
2023-03-27 11:25:46 +0300

    changes

--- src/components/slider/cardSlider.tsx
+++ src/components/slider/cardSlider.tsx
@@ -50,9 +50,9 @@ export const CardSlider = (props: CardSliderProps) => {
       <h1 className="text-primary text-lg p-4">
         <strong>Online Courses</strong>
       </h1>
-      <Slider className="py-5 w-full flex" {...settings}>
+      <Slider className="pb-5 w-full flex" {...settings}>
         {props.courseCard?.map((items) => (
-          <div className=" flex justify-between px-5 py-1">
+          <div className=" flex justify-between px-5 pb-1">
             <CourseCard className="w-full shadow " {...items} />
           </div>
         ))}
