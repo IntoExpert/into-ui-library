63c5b23f14d3156bc725dee4d41847d6d29f23b0
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-28 12:47:42 +0200

    Export notifications icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,4 @@
+import { NotificationsIcon } from "./notificationsIcon/notificationsIcon";
 export * from "./iconApple/iconApple";
 export * from "./IconAvailablity/IconAvailablity";
 export * from "./iconButton/iconButton";
@@ -61,3 +62,4 @@ export * from "./iconCircleAdd/circleAdd";
 export * from "./iconCalenderMonthlyView/iconCalenderMonthlyView";
 export * from "./calenderWeeklySchedule/calenderWeeklyScheduleIcon";
 export * from "./pauseIcon/pauseIcon";
+export * from "./notificationsIcon/notificationsIcon";
