ef458d629e971074f2cee0276c3dbe710aea4401
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-03-31 12:48:13 +0200

    Education fix

--- src/components/user/publicProfile/education.tsx
+++ src/components/user/publicProfile/education.tsx
@@ -11,33 +11,33 @@ export interface EducationProps extends BiographyProps {
   }[];
 }
 export const Education = (props: EducationProps) => {
-    const length = props.body?.length;
+  const length = props.body?.length;
 
-    return (
-      <div className={`${props.className && " "} shadow rounded-lg`}>
-        <Biography title={props.title} className="shadow-none" />
-        {props.body?.map((item, index) => (
-          <div className="text-[10px]" key={index}>
-            {" "}
-            <div className="flex justify-between px-3 pb-3 ">
-              <div>
-                <strong className="py-2 text-[13px]">
-                  {item.universityName}
-                </strong>
-                <p className="opacity-60">{item.degree}</p>
-                <p className="opacity-60"> {item.location}</p>
-              </div>
-              <div className="flex my-auto">
-                {item.icon}{" "}
-                <p className="text-primary px-2">
-                  {" "}
-                  <strong>{item.iconDescription}</strong>
-                </p>
-              </div>
+  return (
+    <div className={`${props.className ?? " "} shadow rounded-lg`}>
+      <Biography title={props.title} className="shadow-none" />
+      {props.body?.map((item, index) => (
+        <div className="text-[10px]" key={index}>
+          {" "}
+          <div className="flex justify-between px-3 pb-3 ">
+            <div>
+              <strong className="py-2 text-[13px]">
+                {item.universityName}
+              </strong>
+              <p className="opacity-60">{item.degree}</p>
+              <p className="opacity-60"> {item.location}</p>
             </div>
-            {index !== length - 1 && <hr className="mx-3" />}
+            <div className="flex my-auto">
+              {item.icon}{" "}
+              <p className="text-primary px-2">
+                {" "}
+                <strong>{item.iconDescription}</strong>
+              </p>
+            </div>
           </div>
-        ))}
-      </div>
-    );
+          {index !== (length ?? 1) - 1 && <hr className="mx-3" />}
+        </div>
+      ))}
+    </div>
+  );
 };
