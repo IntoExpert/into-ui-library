c34c5e62ce87c437220ee9b2688b0109bedd5701
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-04-15 21:03:09 +0300

    Add girl on laptop image

---
+++ src/components/common/girlOnLaptopImage/girlOnLaptop.tsx
@@ -0,0 +1,26 @@
+export const GirlOnLaptopImage = () => <svg xmlns="http://www.w3.org/2000/svg" width="305.693" height="235.927" viewBox="0 0 305.693 235.927">
+    <g id="Group_12715" data-name="Group 12715" transform="translate(-242.971 -153.869)">
+        <path id="Path_71790" data-name="Path 71790" d="M394.855,154.369c-34.046,0-61.646,29.916-61.646,66.818v89.487H456.5V221.187C456.5,184.285,428.9,154.369,394.855,154.369Z" fill="#f6bd60" stroke="#333" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" />
+        <ellipse id="Ellipse_1597" data-name="Ellipse 1597" cx="4.197" cy="6.261" rx="4.197" ry="6.261" transform="translate(344.607 232.546)" fill="#fdcc99" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1" />
+        <path id="Path_71791" data-name="Path 71791" d="M445.321,238.807c0,3.458-.739,6.261-4.2,6.261s-4.2-2.8-4.2-6.261.738-6.261,4.2-6.261S445.321,235.349,445.321,238.807Z" fill="#fdcc99" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1" />
+        <path id="Path_71792" data-name="Path 71792" d="M438.956,223.4a180.628,180.628,0,0,0-19.878-.737c-24.273,0-24.273,6.864-48.546,6.864-7.978,0-14.331-2.693-19.88-5.633a76.777,76.777,0,0,0-.4,7.822c0,33.222,19.97,60.153,44.605,60.153s44.6-26.931,44.6-60.153a76.766,76.766,0,0,0-.45-8.314C438.991,223.4,438.974,223.4,438.956,223.4Z" fill="#fddabf" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1" />
+        <g id="Group_12711" data-name="Group 12711">
+            <circle id="Ellipse_1598" data-name="Ellipse 1598" cx="3.458" cy="3.458" r="3.458" transform="translate(373.387 236.135)" fill="#333" />
+            <g id="Group_12710" data-name="Group 12710">
+                <path id="Path_71793" data-name="Path 71793" d="M372.1,233.522a11.541,11.541,0,0,1,9.494,0" fill="none" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1" />
+            </g>
+        </g>
+        <g id="Group_12713" data-name="Group 12713">
+            <circle id="Ellipse_1599" data-name="Ellipse 1599" cx="3.458" cy="3.458" r="3.458" transform="translate(409.406 236.135)" fill="#333" />
+            <g id="Group_12712" data-name="Group 12712">
+                <path id="Path_71794" data-name="Path 71794" d="M408.117,233.522a11.541,11.541,0,0,1,9.494,0" fill="none" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1" />
+            </g>
+        </g>
+        <rect id="Rectangle_6947" data-name="Rectangle 6947" width="204.923" height="135.914" rx="6.773" transform="translate(293.067 253.382)" fill="#3f94f3" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1" />
+        <path id="Path_71795" data-name="Path 71795" d="M401.563,310.674a6.708,6.708,0,1,1-6.708-6.708A6.708,6.708,0,0,1,401.563,310.674Z" fill="#f3f4f5" stroke="#fff" stroke-miterlimit="10" stroke-width="1" />
+        <g id="Group_12714" data-name="Group 12714" transform="translate(42.402)">
+            <path id="Path_71804" data-name="Path 71804" d="M44.858,0H0" transform="translate(443.231 389.296)" fill="none" stroke="#333" stroke-linecap="round" stroke-width="1" stroke-dasharray="18.372 0 27.559 0" />
+            <line id="Line_3054" data-name="Line 3054" x1="304.693" transform="translate(201.068 389.295)" fill="none" stroke="#333" stroke-linecap="round" stroke-miterlimit="10" stroke-width="1" />
+        </g>
+    </g>
+</svg>;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,1 +1,2 @@
 export * from "./uiElement";
+export * from "./girlOnLaptopImage/girlOnLaptop";
