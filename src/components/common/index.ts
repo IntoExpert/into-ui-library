bfd2af86145535d5633f34ee0a66e4c72f5bbc77
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-04-15 23:03:26 +0300

    Confetti and no search result images

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,4 @@
 export * from "./uiElement";
 export * from "./girlOnLaptopImage/girlOnLaptop";
+export * from "./confettiImage/confettiImage";
+export * from "./noSearchResultImage/noSearchResultImage";
