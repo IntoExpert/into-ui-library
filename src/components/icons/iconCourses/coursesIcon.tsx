1200d9c166a1c25cc164b57bcdc5e4f7a3ce20e2
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-04-17 02:39:34 +0300

    Add icon props for courses icon

--- src/components/icons/iconCourses/coursesIcon.tsx
+++ src/components/icons/iconCourses/coursesIcon.tsx
@@ -1,13 +1,11 @@
-import { UiElementProps } from "../..";
-export interface CoursesIconProps extends UiElementProps { }
+import { IconProps } from "../iconProps";
 
-export const CoursesIcon = (props: CoursesIconProps) => {
+export const CoursesIcon = ({ size = 16, className = 'stroke-black' }: IconProps) => {
   return (
-    <div className={props.className ?? " "}>
+    <div className={`${className}`}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="16.541"
-        height="20.672"
+        width={16}
         viewBox="0 0 16.541 20.672"
       >
         <g id="ebook" transform="translate(0.15 0.15)">
@@ -27,7 +25,6 @@ export const CoursesIcon = (props: CoursesIconProps) => {
                   data-name="Path 71624"
                   d="M18.311,218.857H4.113a1.707,1.707,0,0,1-1.705-1.705v-1.768a1.707,1.707,0,0,1,1.705-1.705h14.2a.337.337,0,0,1,.338.338v4.5A.337.337,0,0,1,18.311,218.857Zm-14.2-4.5a1.031,1.031,0,0,0-1.03,1.03v1.768a1.031,1.031,0,0,0,1.03,1.03h13.86v-3.827H4.113Z"
                   transform="translate(-2.408 -213.678)"
-                  stroke="#000"
                   strokeWidth="0.3"
                 />
               </g>
@@ -43,7 +40,6 @@ export const CoursesIcon = (props: CoursesIconProps) => {
                   data-name="Path 71625"
                   d="M2.746,17.236a.337.337,0,0,1-.338-.338V1.705A1.707,1.707,0,0,1,4.113,0h14.2a.338.338,0,1,1,0,.675H4.113a1.031,1.031,0,0,0-1.03,1.03V16.9a.337.337,0,0,1-.338.338Z"
                   transform="translate(-2.408 0)"
-                  stroke="#000"
                   strokeWidth="0.3"
                 />
               </g>
@@ -59,7 +55,6 @@ export const CoursesIcon = (props: CoursesIconProps) => {
                   data-name="Path 71626"
                   d="M221.661,5.271a.337.337,0,0,1-.338-.338V.338a.338.338,0,0,1,.675,0v4.6A.337.337,0,0,1,221.661,5.271Z"
                   transform="translate(-221.323 0)"
-                  stroke="#000"
                   strokeWidth="0.3"
                 />
               </g>
@@ -79,7 +74,6 @@ export const CoursesIcon = (props: CoursesIconProps) => {
                   data-name="Path 71627"
                   d="M221.661,183.384c-.187,0-.338-.5-.338-1.113v-9.963c0-.617.151-1.113.338-1.113s.338.5.338,1.113v9.963C222,182.884,221.848,183.384,221.661,183.384Z"
                   transform="translate(-221.323 -171.194)"
-                  stroke="#000"
                   strokeWidth="0.3"
                 />
               </g>
@@ -95,7 +89,6 @@ export const CoursesIcon = (props: CoursesIconProps) => {
                   data-name="Path 71628"
                   d="M165.119,239.988h-4.328a.338.338,0,1,1,0-.675h4.328a.338.338,0,0,1,0,.675Z"
                   transform="translate(-160.453 -239.313)"
-                  stroke="#000"
                   strokeWidth="0.3"
                 />
               </g>
@@ -111,7 +104,6 @@ export const CoursesIcon = (props: CoursesIconProps) => {
                   data-name="Path 71629"
                   d="M127.16,239.988h-.975a.338.338,0,1,1,0-.675h.975a.338.338,0,0,1,0,.675Z"
                   transform="translate(-125.847 -239.313)"
-                  stroke="#000"
                   strokeWidth="0.3"
                 />
               </g>
