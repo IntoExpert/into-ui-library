cfceec26c79f905be6d6c031efe3269562f08d91
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-04-17 03:37:51 +0300

    Icon fix

--- src/components/icons/iconDashboard/dashboardIcon.tsx
+++ src/components/icons/iconDashboard/dashboardIcon.tsx
@@ -8,7 +8,7 @@ export const DashboardIcon = ({ size = 19, className = '' }: IconProps) => {
         id="Component_455_1"
         data-name="Component 455 – 1"
         xmlns="http://www.w3.org/2000/svg"
-        width={19}
+        width={size}
         viewBox="0 0 19.402 19.402"
       >
         <path
