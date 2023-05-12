078117cc673733a0294b467fbf1d53066b636e5e
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-05-12 10:19:48 +0300

    Add error message to the phone input

--- src/components/form/input/phone/phoneInput.tsx
+++ src/components/form/input/phone/phoneInput.tsx
@@ -1,13 +1,20 @@
-import PhoneInput2, { PhoneInputProps } from 'react-phone-input-2';
+import PhoneInput2, { PhoneInputProps as PhoneInput2Props } from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css'
+
+export interface PhoneInputProps extends PhoneInput2Props {
+    errormessage?: string;
+}
 
 
 export const PhoneInput = ({ ...props }: PhoneInputProps) => {
-    return <PhoneInput2
-        excludeCountries={["il", "IL"]}
-        {...props}
-        containerClass={`!border-secondary h-14 ${props.containerClass ?? ''}`}
-        inputClass={`!border-secondary !h-14 ${props.inputClass ?? ''}`}
-        buttonClass={`!border-secondary !bg-surface h-14 !border-r-0 ${props.buttonClass ?? ''}`}
-    />
+    return <div>
+        <PhoneInput2
+            excludeCountries={["il", "IL"]}
+            {...props}
+            containerClass={`!border-secondary h-14 ${props.containerClass ?? ''}`}
+            inputClass={`!border-secondary !h-14 ${props.inputClass ?? ''}`}
+            buttonClass={`!border-secondary !bg-surface h-14 !border-r-0 ${props.buttonClass ?? ''}`}
+        />
+        {props.errormessage && <p className="text-error text-xs italic mt-1">{props.errormessage}</p>}
+    </div>
 }
