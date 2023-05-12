5e9770c4fcb35689129987a53d9ae079444029b0
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-05-12 16:15:49 +0300

    Disabled phone input appearance

--- src/components/form/input/phone/phoneInput.tsx
+++ src/components/form/input/phone/phoneInput.tsx
@@ -12,8 +12,10 @@ export const PhoneInput = ({ ...props }: PhoneInputProps) => {
             excludeCountries={["il", "IL"]}
             {...props}
             containerClass={`!border-secondary h-14 w-full ${props.containerClass ?? ''}`}
-            inputClass={`!border-secondary !h-14 ${props.inputClass ?? ''}`}
-            buttonClass={`!border-secondary !bg-surface h-14 !border-r-0 ${props.buttonClass ?? ''}`}
+            inputClass={`!border-secondary !h-14 disabled:!bg-gray-100 ${props.inputClass ?? ''}`}
+            buttonClass={`!border-secondary !bg-surface h-14 !border-r-0 
+                ${props.disabled ? '!bg-gray-100' : ''} 
+                ${props.buttonClass ?? ''}`}
         />
         {props.errormessage && <p className="text-error text-xs italic mt-1">{props.errormessage}</p>}
     </div>
