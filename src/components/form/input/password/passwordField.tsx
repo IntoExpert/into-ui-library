30a43733f1212b98f6a96160a0a40c21b99850c4
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-05-15 10:56:38 +0300

    Show hide password component fixes

--- src/components/form/input/password/passwordField.tsx
+++ src/components/form/input/password/passwordField.tsx
@@ -11,19 +11,24 @@ export const PasswordField = forwardRef<HTMLInputElement, PasswordFieldProps>(({
 
     const [showPassword, setShowPassword] = useState(show);
 
-    useEffect(() => {
-        onShowPasswordToggle?.(!showPassword)
-    }, [showPassword, onShowPasswordToggle]);
+
+    const handleShowPassword = useCallback(() => {
+        setShowPassword(prev => {
+            const newValue = !prev;
+            onShowPasswordToggle?.(newValue)
+            return newValue;
+        })
+    }, [onShowPasswordToggle]);
 
     useEffect(() => setShowPassword(show), [setShowPassword, show]);
 
     const ShowPasswordIcon = useCallback(() => {
-        return <div className={`cursor-pointer`} onClick={() => setShowPassword(prev => !prev)}>
+        return <div className={`cursor-pointer`} onClick={handleShowPassword}>
             <EyeIcon className={`fill-gray-400`} />
             <div className={`absolute top-1/3 rounded-2xl w-full h-1 bg-gray-300 rotate-45 transition-all
             ${showPassword ? `` : 'max-w-0'}`}></div>
         </div>
-    }, [showPassword]);
+    }, [showPassword, handleShowPassword]);
 
     return <InputField {...props} ref={ref} type={showPassword ? 'text' : 'password'} inputPostfix={<ShowPasswordIcon />} />
 });
