2eae21a91633e45f0aebfb8e16f3d202b43a45c9
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-05-16 14:51:01 +0300

    Fix avatar

--- src/components/image/avatar/avatar.tsx
+++ src/components/image/avatar/avatar.tsx
@@ -33,11 +33,13 @@ export interface AvatarProps extends ImageProps {
 export const Avatar = (props: AvatarProps) => {
 
     const avatatDefaultSrc = useMemo(() => {
+        if (props.src) return props.src;
+
         if (props.firstName && props.lastName) {
             return undefined;
         }
 
-        return props.src ?? "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg";
+        return "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg";
 
     }, [props.src, props.firstName, props.lastName]);
 
