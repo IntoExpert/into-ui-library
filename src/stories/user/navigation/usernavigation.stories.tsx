e2e6a2061b18d73a5785d013d4593017eaf7abe1
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-05-17 12:54:12 +0300

    User navigation fixes

--- src/stories/user/navigation/usernavigation.stories.tsx
+++ src/stories/user/navigation/usernavigation.stories.tsx
@@ -13,8 +13,10 @@ export const userNavigation = Template.bind({});
 userNavigation.args = {
     avatar: {
         alt: "Face portrait",
+        firstName: 'Motasem',
+        lastName: 'Ghozlan',
         src: "https://cdn.dribbble.com/users/544003/screenshots/18063915/media/13befc9277da37247dad63ddb46c235f.jpg?compress=1&resize=400x300",
-        className: "h-20 w-20",
+        className: "!w-20 !h-auto",
         label: {
             name: "Motasem Ghz",
         },
