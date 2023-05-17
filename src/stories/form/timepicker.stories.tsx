449cae154e68bbcd0776230626f4be36ab1363a1
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-05-17 10:22:56 +0300

    Fix try

--- src/stories/form/timepicker.stories.tsx
+++ src/stories/form/timepicker.stories.tsx
@@ -2,6 +2,13 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { DatePicker } from "../../components/form/input/date/datePicker";
 import { TimePicker } from "../../components/form/input/date/datePicker/timePicker";
 
+import { enGB, arSA } from "date-fns/locale";
+import { registerLocale } from "react-datepicker";
+
+
+registerLocale('en', enGB);
+registerLocale('ar', arSA);
+
 export default {
     title: 'Form/Timepicker',
     component: DatePicker,
@@ -17,6 +24,7 @@ defaultState.args = {
 
 export const DateTimePicker = Template.bind({});
 DateTimePicker.args = {
+    locale: 'ar',
     showTimeSelectOnly: false,
     showTimeSelect: true,
     dateFormat: 'yyyy-MM-dd hh:mm aa',
