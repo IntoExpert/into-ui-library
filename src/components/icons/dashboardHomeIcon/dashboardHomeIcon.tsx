cd4170fb70c02583de3ab8db3dfbf24698337094
hasan <hasan>
2023-05-23 11:00:26 +0300

    changes

--- src/components/icons/dashboardHomeIcon/dashboardHomeIcon.tsx
+++ src/components/icons/dashboardHomeIcon/dashboardHomeIcon.tsx
@@ -3,7 +3,7 @@ import { IconProps } from "../iconProps";
 export const DashboardHomeIcon = ({ size = 14, className = 'fill-secondary' }: IconProps) =>
     <svg className={`${className}`} xmlns="http://www.w3.org/2000/svg" width={size} viewBox="0 0 20.372 21.2">
         <g id="Component_889_4" data-name="Component 889 – 4" transform="translate(0.6 0.6)">
-            <path id="Stroke_1" data-name="Stroke 1" d="M0,.5H5.815" transform="translate(6.679 13.635)" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" />
-            <path id="Stroke_2" data-name="Stroke 2" d="M0,11.713c0-5.631.614-5.238,3.919-8.3C5.365,2.246,7.615,0,9.558,0s4.237,2.235,5.7,3.41c3.305,3.065,3.918,2.672,3.918,8.3C19.172,20,17.213,20,9.586,20S0,20,0,11.713Z" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" />
+            <path id="Stroke_1" data-name="Stroke 1" d="M0,.5H5.815" transform="translate(6.679 13.635)" stroke-linecap="round" stroke-linejoin="round" strokeWidth="1.2" />
+            <path id="Stroke_2" data-name="Stroke 2" d="M0,11.713c0-5.631.614-5.238,3.919-8.3C5.365,2.246,7.615,0,9.558,0s4.237,2.235,5.7,3.41c3.305,3.065,3.918,2.672,3.918,8.3C19.172,20,17.213,20,9.586,20S0,20,0,11.713Z" stroke-linecap="round" stroke-linejoin="round" strokeWidth="1.2" />
         </g>
     </svg>
