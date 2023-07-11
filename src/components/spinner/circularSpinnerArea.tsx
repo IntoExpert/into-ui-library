9eec13340d5229a9aaca98cc06b8b786e3a94d48
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-07-11 15:39:14 +0300

    Add circular spinner area

---
+++ src/components/spinner/circularSpinnerArea.tsx
@@ -0,0 +1,8 @@
+import { CircularSpinner } from "./circularSpinner";
+
+export const CircularSpinnerArea = () => {
+
+    return <div className={`w-full flex justify-center items-center`}>
+        <CircularSpinner />
+    </div>;
+}
