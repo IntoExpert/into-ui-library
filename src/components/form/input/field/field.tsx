49fa420bb619817e732f484675e8fdeb6c5d15a2
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-09-18 17:11:51 +0300

    Placeholder sizes

--- src/components/form/input/field/field.tsx
+++ src/components/form/input/field/field.tsx
@@ -82,7 +82,7 @@ export interface InputFieldProps extends UiElementProps {
 }
 
 interface InputFieldState {
-    value?: string;
+  value?: string;
 }
 
 /**
@@ -93,97 +93,97 @@ interface InputFieldState {
  */
 export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(({ value, onPast, size, ...props }, ref) => {
 
-    const [state, setState] = useState<InputFieldState>({ value: value ?? props.defaultValue });
+  const [state, setState] = useState<InputFieldState>({ value: value ?? props.defaultValue });
 
-    useEffect(() => {
-        setState(prevState => ({ ...prevState, value: value }));
-    }, [value])
+  useEffect(() => {
+    setState(prevState => ({ ...prevState, value: value }));
+  }, [value])
 
-    const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value;
-        setState(prevState => ({ prevState, value }));
-        props.onChange?.(event);
-    }
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setState(prevState => ({ prevState, value }));
+    props.onChange?.(event);
+  }
 
 
-    switch (props.type) {
-        case "radio":
-        case "checkbox":
-            const isRadio = props.type === 'radio';
-            const radioClasses = `appearance-none rounded-full border border-gray-300 bg-white checked:bg-white 
+  switch (props.type) {
+    case "radio":
+    case "checkbox":
+      const isRadio = props.type === 'radio';
+      const radioClasses = `appearance-none rounded-full border border-gray-300 bg-white checked:bg-white 
             checked:border-secondary checked:border-4 focus:outline-secondary transition duration-300 
             cursor-pointer focus:ring-2 focus:ring-secondary focus:shadow-none`;
-            const checkBoxClasses = `rounded border border-gray-400 checked:bg-red-400 checked:after:content-['✓'] 
+      const checkBoxClasses = `rounded border border-gray-400 checked:bg-red-400 checked:after:content-['✓'] 
             checked:border-red-400 after:text-surface after:absolute after:0 after:w-full after:flex after:justify-center 
             after:items-center after:text-xs appearance-none focus:outline-secondary after:pointer-events-none`;
 
-            return (
-              <div
-                className={`flex justify-start align-baseline ${props.containerClassName}`}
-              >
-                <input
-                  {...props}
-                  className={`relative h-4 w-4 ${
-                    isRadio ? radioClasses : checkBoxClasses
-                  }
+      return (
+        <div
+          className={`flex justify-start align-baseline ${props.containerClassName}`}
+        >
+          <input
+            {...props}
+            className={`relative h-4 w-4 ${isRadio ? radioClasses : checkBoxClasses
+              }
                          ${props.className ?? ""}`}
-                  value={props.name}
-                  onClick={(e) => {
-                    const newValue = state.value ? "" : e.currentTarget.value;
-                    setState((prevState) => ({
-                      ...prevState,
-                      value: newValue,
-                    }));
-                  }}
-                  ref={ref}
-                  checked={props.checked}
-                />
-                <InputLabel
-                  inputId={props.id}
-                  content={props.label}
-                  className="mx-2 inline-block"
-                />
-              </div>
-            );
-        default:
-            return <div className={`text-start ${props.containerClassName}`}>
-                <InputLabel inputId={props.id} content={props.label} className="block mb-2" />
-                <div className={`relative`}>
-                    {props.inputPrefix
-                        ? <div className={`absolute top-1/2 -translate-y-1/2 ${props.dir === 'rtl' ? 'right-4' : 'left-4'}`}>
-                            {props.inputPrefix}
-                        </div>
-                        : null}
-                    <input
-                        {...props}
-                        className={`shadow appearance-none border w-full
+            value={props.name}
+            onClick={(e) => {
+              const newValue = state.value ? "" : e.currentTarget.value;
+              setState((prevState) => ({
+                ...prevState,
+                value: newValue,
+              }));
+            }}
+            ref={ref}
+            checked={props.checked}
+          />
+          <InputLabel
+            inputId={props.id}
+            content={props.label}
+            className="mx-2 inline-block"
+          />
+        </div>
+      );
+    default:
+      return <div className={`text-start ${props.containerClassName}`}>
+        <InputLabel inputId={props.id} content={props.label} className="block mb-2" />
+        <div className={`relative`}>
+          {props.inputPrefix
+            ? <div className={`absolute top-1/2 -translate-y-1/2 ${props.dir === 'rtl' ? 'right-4' : 'left-4'}`}>
+              {props.inputPrefix}
+            </div>
+            : null}
+          <input
+            {...props}
+            className={`shadow appearance-none border w-full
                             rounded ${inputHeightClassDurToSize(size)} px-3 text-gray-700 leading-tight 
                             focus:outline-none focus:shadow-outline
-                            placeholder:!text-gray-500 placeholder:font-light disabled:!bg-gray-100
-                            ${props.errormessage ? 'border-error' : 'border-secondary'}
-                            ${props.className ?? ''}
-                            ${props.inputPostfix ? props.dir === 'rtl' ? 'pl-12' : 'pr-12' : ''}
-                            ${props.inputPrefix ? props.dir === 'ltr' ? 'pl-12' : 'pr-12' : ''}`
-                        }
-                        onChange={handleOnChange}
-                        onPaste={onPast}
-                        value={state.value}
-                        ref={ref}
-                    />
-                    {props.inputPostfix ? <div className={`absolute top-1/2 -translate-y-1/2 ${props.dir === 'rtl' ? 'left-4' : 'right-4'}`}>
-                        {props.inputPostfix}
-                    </div> : null}
-                    {/* Featured placeholder */}
-                    {
-                        props.featuredPlaceholder && !state.value
-                            ? <div className={`absolute pointer-events-none top-1/2 -translate-y-1/2 left-3 text-gray-500 ${props.direction === 'rtl' ? "right-0" : ""}`}>
-                                {props.featuredPlaceholder}
-                            </div>
-                            : null
-                    }
-                </div>
+                            placeholder:!text-gray-500 placeholder:font-light ${size === 'sm' ? 'text-xs' : ''}
+          disabled:!bg-gray-100
+          ${props.errormessage ? 'border-error' : 'border-secondary'}
+          ${props.className ?? ''}
+          ${props.inputPostfix ? props.dir === 'rtl' ? 'pl-12' : 'pr-12' : ''}
+          ${props.inputPrefix ? props.dir === 'ltr' ? 'pl-12' : 'pr-12' : ''}`
+            }
+            onChange={handleOnChange}
+            onPaste={onPast}
+            value={state.value}
+            ref={ref}
+          />
+          {props.inputPostfix ? <div className={`absolute top-1/2 -translate-y-1/2 ${props.dir === 'rtl' ? 'left-4' : 'right-4'}`}>
+            {props.inputPostfix}
+          </div> : null}
+          {/* Featured placeholder */}
+          {
+            props.featuredPlaceholder && !state.value
+              ? <div className={`absolute pointer-events-none top-1/2 -translate-y-1/2 left-3 text-gray-500 ${props.direction === 'rtl' ? "right-0" : ""}`}>
+                {props.featuredPlaceholder}
+              </div>
+              : null
+          }
+        </div>
 
-                {props.errormessage && <p className="text-error text-xs italic mt-1">{props.errormessage}</p>}
-            </div>;
-    };
+        {props.errormessage && <p className="text-error text-xs italic mt-1">{props.errormessage}</p>}
+      </div>;
+  };
 })
