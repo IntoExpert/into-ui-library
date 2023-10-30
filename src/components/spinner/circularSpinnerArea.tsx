1bbe7e1a15878e4a7e191399d6a848968345aaed
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-10-30 17:04:13 +0200

    Area loader fixes

--- src/components/spinner/circularSpinnerArea.tsx
+++ src/components/spinner/circularSpinnerArea.tsx
@@ -2,7 +2,7 @@ import { CircularSpinner } from "./circularSpinner";
 
 export const CircularSpinnerArea = () => {
 
-    return <div className={`w-full flex justify-center items-center`}>
+    return <div className={`w-full h-full p-10 flex justify-center items-center`}>
         <CircularSpinner />
     </div>;
 }
