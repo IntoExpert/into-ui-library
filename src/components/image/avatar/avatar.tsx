97a6b232152e0278eba94b75273bbceafa8a9927
Into Expert <intoexpert@Intos-MBP.localdomain>
2024-03-01 09:22:59 +0200

    adding crossOrigin to Image component

--- src/components/image/avatar/avatar.tsx
+++ src/components/image/avatar/avatar.tsx
@@ -2,67 +2,81 @@ import { useMemo } from "react";
 import { ImageProps } from "../image";
 
 export interface AvatarProps extends ImageProps {
+  /**
+   * User label
+   */
+  firstName?: string;
+  lastName?: string;
+  label?: {
     /**
-     * User label
-     */
-    firstName?: string;
-    lastName?: string;
-    label?: {
-        /**
      * Show user name
      */
-        name?: string;
-        /**
-         * Class name
-         */
-        classname?: string;
-        /**
-         * Label place
-         */
-        place?: "bottom" | "end"
-    }
+    name?: string;
     /**
-     * Class name to add for the container of the image and its label
+     * Class name
      */
-    containerClassName?: string;
-};
+    classname?: string;
+    /**
+     * Label place
+     */
+    place?: "bottom" | "end";
+  };
+  /**
+   * Class name to add for the container of the image and its label
+   */
+  containerClassName?: string;
+}
 
 /**
  * User avatar
  */
 export const Avatar = (props: AvatarProps) => {
+  const avatatDefaultSrc = useMemo(() => {
+    if (props.src) return props.src;
 
-    const avatatDefaultSrc = useMemo(() => {
-        if (props.src) return props.src;
+    if (props.firstName && props.lastName) {
+      return undefined;
+    }
 
-        if (props.firstName && props.lastName) {
-            return undefined;
-        }
+    return "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg";
+  }, [props.src, props.firstName, props.lastName]);
 
-        return "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg";
-
-    }, [props.src, props.firstName, props.lastName]);
-
-    return (
-        <div className={`flex justify-center items-center 
+  return (
+    <div
+      className={`flex justify-center items-center 
             ${props.label?.place === "bottom" ? "flex-col" : ""} 
-            ${props.className ?? ''}`}>
-            {avatatDefaultSrc ? <img {...props}
-                alt={props.alt ?? props.label?.name ?? ''}
-                src={avatatDefaultSrc}
-                title={props.label?.name ?? props.alt ?? ''}
-                className={`rounded-full object-cover w-10 h-10 aspect-square ${props.className ?? ''}`}
-                loading={props.loadingMethod ?? "lazy"} />
-                : <h4
-                    className={`w-10 h-10 aspect-square bg-gray-200 flex justify-center items-center rounded-full 
-                        font-bold text-sm ${props.className ?? ''}`}
-                    dir="auto">
-                    {props.firstName?.charAt(0)?.toUpperCase()} {props.lastName?.charAt(0)?.toUpperCase()}
-                </h4>}
-            {props.label?.name ?
-                <h4 dir="auto" className={`mx-2 my-2 text-sm font-bold text-gray-500 whitespace-nowrap ${props.label?.classname}`}>
-                    {props.label?.name}
-                </h4> : null}
-        </div>
-    );
-}
+            ${props.className ?? ""}`}
+    >
+      {avatatDefaultSrc ? (
+        <img
+          {...props}
+          alt={props.alt ?? props.label?.name ?? ""}
+          src={avatatDefaultSrc}
+          title={props.label?.name ?? props.alt ?? ""}
+          className={`rounded-full object-cover w-10 h-10 aspect-square ${
+            props.className ?? ""
+          }`}
+          loading={props.loadingMethod ?? "lazy"}
+          crossOrigin=""
+        />
+      ) : (
+        <h4
+          className={`w-10 h-10 aspect-square bg-gray-200 flex justify-center items-center rounded-full 
+                        font-bold text-sm ${props.className ?? ""}`}
+          dir="auto"
+        >
+          {props.firstName?.charAt(0)?.toUpperCase()}{" "}
+          {props.lastName?.charAt(0)?.toUpperCase()}
+        </h4>
+      )}
+      {props.label?.name ? (
+        <h4
+          dir="auto"
+          className={`mx-2 my-2 text-sm font-bold text-gray-500 whitespace-nowrap ${props.label?.classname}`}
+        >
+          {props.label?.name}
+        </h4>
+      ) : null}
+    </div>
+  );
+};
