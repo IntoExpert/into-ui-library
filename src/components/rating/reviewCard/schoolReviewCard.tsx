6f332945bdc3e336d8a0993978c061c2ad6f8b11
Motasem Ghozlan <Motasem.go.26@gmail.com>
2024-07-26 11:30:41 +0300

    School review card

--- src/components/rating/reviewCard/schoolReviewCard.tsx
+++ src/components/rating/reviewCard/schoolReviewCard.tsx
@@ -1,28 +1,36 @@
 import { DateTime } from "luxon";
 import { Card } from "../../card";
 import { getRatingFaceByRating } from "../faces/ratingFaces";
+import { EditIcon } from "../../icons";
 
 export interface schoolReviewCardProps {
     rating?: number;
     date?: DateTime;
     description?: string;
+    isEditable?: boolean;
+    onEdit?: () => void;
 }
 
-export function SchoolReviewCard({ rating = 1, date, description }: schoolReviewCardProps) {
+export function SchoolReviewCard({ rating = 1, date, description, isEditable, onEdit }: schoolReviewCardProps) {
 
     const RatingFace = getRatingFaceByRating(rating);
 
-    return <Card className={`p-4`}>
-        <div className="flex gap-8">
-            <div className="inline-flex items-center">
-                <RatingFace isFilled />
+    return <Card className={`relative p-8`}>
+        <>
+            {isEditable ? <div className="absolute top-2 right-2">
+                <button title="Edit" type="button" className="px-0.5" onClick={onEdit}><EditIcon /></button>
+            </div> : null}
+            <div className="flex gap-8">
+                <div className="inline-flex items-center">
+                    <RatingFace isFilled />
+                </div>
+                <div>
+                    <p className={`text-xs text-gray-400`}>{date?.toLocaleString({ dateStyle: 'medium' })}</p>
+                    <p className="text-sm">
+                        {description}
+                    </p>
+                </div>
             </div>
-            <div>
-                <p className={`text-xs text-gray-400`}>{date?.toLocaleString({ dateStyle: 'medium' })}</p>
-                <p className="text-sm">
-                    {description}
-                </p>
-            </div>
-        </div>
+        </>
     </Card>
 }
--- src/stories/rating/schoolRatingCard.stories.tsx
+++ src/stories/rating/schoolRatingCard.stories.tsx
@@ -17,3 +17,14 @@ SchoolReviewCardRating5.args = {
     date: DateTime.now(),
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
 };
+
+export const EditableSchoolReviewCardRating5 = Template.bind({});
+EditableSchoolReviewCardRating5.args = {
+    rating: 5,
+    date: DateTime.now(),
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+    isEditable: true,
+    onEdit: () => {
+        window.confirm('Are you sure you want to edit');
+    },
+};
