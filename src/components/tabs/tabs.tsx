95d36595f639058d13ad1e71496e59ca22e8e22a
Motasem Ghozlan <Motasem.go.26@gmail.com>
2024-09-12 14:02:16 +0300

    On tap change function

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -4,6 +4,7 @@ export interface TabsProps {
   tabs: TabProps[];
   tabsContainerClassName?: string;
   activeTabIndex?: number;
+  onTapChange?: (id: string) => void,
   className?: string;
 }
 
@@ -23,6 +24,7 @@ export const Tabs = ({
   tabs,
   tabsContainerClassName = "",
   activeTabIndex = 0,
+  onTapChange,
   className,
 }: TabsProps) => {
   const [state, setState] = useState<TabsState>({
@@ -30,8 +32,9 @@ export const Tabs = ({
   });
 
   const setTab = useCallback((id: string) => {
+    onTapChange?.(id);
     setState((prevState) => ({ ...prevState, activeTabId: id }));
-  }, []);
+  }, [onTapChange]);
 
   useEffect(() => {
     setState({
@@ -52,13 +55,11 @@ export const Tabs = ({
               className={`
                       border-b-2 text-gray-400 bg-surface p-1 transition-all text-ellipsis overflow-hidden cursor-pointer
                       ${tab.className ?? ""}
-                      ${
-                        state.activeTabId === tab.id
-                          ? `text-red-300 border-b-red-300 border-b-4 rounded ${
-                              tab.activeClassName ?? ""
-                            }`
-                          : "hover:text-error-200 hover:border-b-red-200"
-                      }`}
+                      ${state.activeTabId === tab.id
+                  ? `text-red-300 border-b-red-300 border-b-4 rounded ${tab.activeClassName ?? ""
+                  }`
+                  : "hover:text-error-200 hover:border-b-red-200"
+                }`}
               onClick={() => setTab(tab.id!)}
             >
               {tab.title}
